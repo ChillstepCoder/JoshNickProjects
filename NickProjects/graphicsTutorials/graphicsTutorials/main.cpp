3c9e835c15975554669b568e084b15e77221d1ba
Nicholas Arnold <Xewulf@gmail.com>
2024-08-31 21:34:58 -0500

    GraphicsTutorials Ver 0.01
    watched through to Episode 2. Currently it doesnt work at all and the window doesnt open due to an error. 0xc000007b.

---
+++ NickProjects/graphicsTutorials/graphicsTutorials/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include "MainGame.h"
+#include <GL/glew.h>
+
+int main(int argc, char** argv) {
+    MainGame mainGame;
+    mainGame.run();
+
+    std::cout << "Enter any key to quit...";
+    int a;
+    std::cin >> a;
+    return 0;
+}
