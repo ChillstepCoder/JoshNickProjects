6ff6325c1fff709b1b39425f7d28e9245d580ec1
JayArnoldProd <jayarnoldproduces@gmail.com>
2024-09-13 21:14:17 -0500

    update zombie game begin enemy movement

--- JoshProjects/GraphicsProject/ZombieGame/Human.cpp
+++ JoshProjects/GraphicsProject/ZombieGame/Human.cpp
@@ -1,6 +1,7 @@
 //Human.cpp
 
 #include "Human.h"
+#include <random>
 
 Human::Human() {
 
@@ -13,5 +14,18 @@ Human::~Human() {
 void Human::update(const std::vector<std::string>& levelData,
   std::vector<Human*>& humans,
   std::vector<Zombie*>& zombies) {
+  
 
+  collideWithLevel(levelData);
+}
+
+void Human::init(float speed, glm::vec2 pos) {
+
+  //static std::mt19937 randomEngine;
+  //randomEngine
+  //static std::uniform_real_distribution<float>(0.01f, 1.0f);
+
+  _speed = speed;
+  _position = pos;
+  _direction =
 }
--- JoshProjects/GraphicsProject/ZombieGame/Human.h
+++ JoshProjects/GraphicsProject/ZombieGame/Human.h
@@ -8,7 +8,13 @@ class Human : public Agent
 public:
   Human();
   virtual ~Human();
+
+  void init(float speed, glm::vec2 pos);
+
   virtual void update(const std::vector<std::string>& levelData,
     std::vector<Human*>& humans,
     std::vector<Zombie*>& zombies) = 0;
+
+private:
+  glm::vec2 _direction;
 };
