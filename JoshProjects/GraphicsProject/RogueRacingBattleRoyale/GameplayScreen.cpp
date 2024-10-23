4623be7e69a70a3e7a097f4723d43855c0eaa30d
JayArnoldProd <jayarnoldproduces@gmail.com>
2024-10-23 04:36:37 -0500

    Build and integrate Box 2D
    built Box2D using terminal
    - added box 2d to game

--- JoshProjects/GraphicsProject/RogueRacingBattleRoyale/GameplayScreen.cpp
+++ JoshProjects/GraphicsProject/RogueRacingBattleRoyale/GameplayScreen.cpp
@@ -1,5 +1,13 @@
 #include "GameplayScreen.h"
+#include <SDL/SDL.h>
+#include <JAGEngine/IMainGame.h>
 
+GameplayScreen::GameplayScreen() {
+  // empty
+}
+GameplayScreen::~GameplayScreen() {
+  // empty
+}
 
 int GameplayScreen::getNextScreenIndex() const {
   return -1;
@@ -19,6 +27,19 @@ void GameplayScreen::destroy() {
 
 void GameplayScreen::onEntry() {
   std::cout << "On Entry\n";
+
+  //b2Vec2 gravity(0.0f, -9.81f);
+  //m_world = std::make_unique<b2World>(gravity);
+
+  //// make the ground
+  //b2BodyDef groundBodyDef;
+  //groundBodyDef.position.Set(0.0f, -10.0f);
+  //b2Body* groundBody = m_world->CreateBody(&groundBodyDef);
+
+  //// make the ground fixture.
+  //b2PolygonShape groundBox;
+  //groundBox.SetAsBox(50.0f, 10.0f);
+  //groundBody->CreateFixture(&groundBody, 0.0f);
 }
 
 void GameplayScreen::onExit() {
@@ -31,4 +52,10 @@ void GameplayScreen::update() {
 
 void GameplayScreen::draw() {
   std::cout << "Draw\n";
+  glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+  glClearColor(1.0f, 0.0f, 0.0f, 1.0f);
+}
+
+void GameplayScreen::checkInput() {
+
 }
--- JoshProjects/GraphicsProject/RogueRacingBattleRoyale/GameplayScreen.h
+++ JoshProjects/GraphicsProject/RogueRacingBattleRoyale/GameplayScreen.h
@@ -1,7 +1,9 @@
 
 #pragma once
 
+
 #include <JAGEngine/IGameScreen.h>
+#include <Box2D/box2d.h>
 #include <iostream>
 
 class GameplayScreen : public JAGEngine::IGameScreen {
@@ -9,13 +11,24 @@ class GameplayScreen : public JAGEngine::IGameScreen {
   ~GameplayScreen();
 
   virtual int getNextScreenIndex() const override;
+
   virtual int getPreviousScreenIndex() const override;
+
   virtual void build() override;
+
   virtual void destroy() override;
+
   virtual void onEntry() override;
+
   virtual void onExit() override;
+
   virtual void update() override;
+
   virtual void draw() override;
 
+private:
+  void checkInput();
+
+  //std::unique_ptr<b2World> m_world;
 };
 
