534932d5959f72969fb76b52745385ebd291c9f4
JayArnoldProd <jayarnoldproduces@gmail.com>
2024-10-28 23:05:53 -0500

    stole Nick's debugDraw code

---
+++ JoshProjects/GraphicsProject/RogueRacingBattleRoyale/DebugDraw.h
@@ -0,0 +1,34 @@
+// DebugDraw.h
+
+#pragma once
+#include <box2d/box2d.h>
+#include <JAGEngine/GLSLProgram.h>
+#include <glm/glm.hpp>
+
+class DebugDraw {
+public:
+    DebugDraw();
+    ~DebugDraw();
+
+    void init();
+    void drawWorld(b2WorldId* world, const glm::mat4& projectionMatrix);
+    void setAlpha(float alpha) { m_alpha = alpha; }
+
+private:
+    // Updated callback signatures to match Box2D 3.0
+    static void drawSegment(b2Vec2 p1, b2Vec2 p2, b2HexColor color, void* context);
+    static void drawPolygon(const b2Vec2* vertices, int vertexCount, b2HexColor color, void* context);
+    static void drawCircle(b2Vec2 center, float radius, b2HexColor color, void* context);
+    static void drawSolidCircle(b2Transform xf, float radius, b2HexColor color, void* context);
+    static void drawSolidPolygon(b2Transform xf, const b2Vec2* vertices, int vertexCount, float radius, b2HexColor color, void* context);
+    static void drawTransform(b2Transform xf, void* context);
+    static void drawPoint(b2Vec2 p, float size, b2HexColor color, void* context);
+    static void drawString(b2Vec2 p, const char* s, void* context);
+    static void setAttrib();
+
+    float m_alpha = 0.1f;
+    JAGEngine::GLSLProgram m_program;
+    GLuint m_vboId = 0;
+    GLuint m_vaoId = 0;
+};
+
